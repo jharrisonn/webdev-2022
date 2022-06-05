4a722e6c17b72a53e351b943c04da28a8dadff25
Jarrisonn <jorge.harrisonn@usp.br>
2022-06-04 21:51:36 -0300

    Added my hackfools project into it

---
+++ hackfools/scripts/sabores.js
@@ -0,0 +1,111 @@
+sabores = { 
+    bases: [
+        "trigo", 
+        "queijo", 
+        "chocolate",
+        "borracha", 
+        "papelao",
+        "abacaxi"
+    ],
+    recheios: [
+        "calabresa", 
+        "pepperoni", 
+        "margherita",
+        "presunto e queijo", 
+        "portuguesa", 
+        "chocolate", 
+        "doce de leite", 
+        "abacaxi", 
+        "galinha"
+    ],
+    molhos: [
+        "ketchup",
+        "tomate",
+        "shoyu",
+        "branco", 
+        "tartaro", 
+        "caesar", 
+        "abacaxi"
+    ],
+    adicionais: [
+        "milho", 
+        "oregano",
+        "ovo",
+        "batata frita", 
+        "ralo", 
+        "abacaxi",
+        "clipe de papel",
+        "sim"
+    ]
+}
+
+precos = {
+        bases: {
+            trigo: 2.0, 
+            queijo: 2.0, 
+            chocolate: 2.0,
+            borracha: 2.0, 
+            papelao: 2.0,
+            abacaxi: 2.0
+        },
+        recheios: {
+            galinha: 2.0,
+            calabresa: 2.0, 
+            pepperoni: 2.0, 
+            margherita: 2.0,
+            presuntoequeijo: 2.0, 
+            portuguesa: 2.0, 
+            chocolate: 2.0, 
+            docedeleite: 2.0, 
+            abacaxi: 2.0, 
+            chocolate: 2.0
+        },
+        molhos: {
+            ketchup: 2.0,
+            tomate: 2.0,
+            shoyu: 2.0,
+            branco: 2.0, 
+            tartaro: 2.0, 
+            caesar: 2.0, 
+            abacaxi: 2.0
+        },
+        adicionais: {
+            milho: 0.5, 
+            oregano: 0.5,
+            ovo: 0.5,
+            batatafrita: 0.5, 
+            ralo: 0.5, 
+            abacaxi: 0.5,
+            clipedepapel: 0.5,
+            sim: 3.5
+        }
+    }
+listaopcoes = [
+    "grau", "coerente", "assada", "queimada", "cabelo", "saliva"
+]
+opcoes = {
+    grau: [
+        "Motoboy não pode dar grau?", 1
+    ],
+    coerente: [
+        "Deseja receber a pizza parecida com o que solicitou?", 12
+    ],
+    assada: [
+        "Deseja que sua pizza seja assada?", 2
+    ],
+    queimada: [
+        "Deseja que sua pizza não esteja queimada?", 2.5
+    ],
+    caixa: [
+        "Deseja que sua pizza tenha uma caixa?", 8
+    ],
+    validade: [
+        "Deseja que os ingredientes não estejam vencidos?", 3
+    ],
+    cabelo: [
+        "Deseja que sua pizza venha sem cabelo?", 1.99
+    ],
+    saliva: [
+        "Deseja saliva na sua pizza?", 1
+    ]
+}
